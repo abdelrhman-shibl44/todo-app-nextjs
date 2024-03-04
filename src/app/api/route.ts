c0459adf2d3b9bb813ccda2f27a5f41cd46fd52a
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-04 09:37:32 +0200

    make post request to create todos

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { ConnectDB } from "@/lib/config/db";
+import { TodoModel } from "@/lib/config/models/TodoModel";
 const LoadDb = async () => {
   await ConnectDB();
 };
@@ -7,7 +8,25 @@ LoadDb();
 
 export async function GET() {
   try {
-    return NextResponse.json("hi");
+    const todos = await TodoModel.find({});
+    return NextResponse.json(todos);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const { title, description } = await req.json();
+  if (!title || !description) {
+    return NextResponse.json("All fields are required");
+  }
+  try {
+    const createdTodo = await TodoModel.create({
+      title,
+      description,
+    });
+
+    return NextResponse.json({ msg: "todo created", createdTodo });
   } catch (err) {
     console.log(err);
   }
