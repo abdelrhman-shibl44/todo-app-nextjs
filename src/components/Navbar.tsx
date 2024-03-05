30ff33e356bc60aa503fa2fde8bf374b459b56ed
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-05 20:30:08 +0200

    create theme toggle component and add it to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   const pathname = usePathname();
 
@@ -13,12 +14,12 @@ const Navbar = () => {
     { key: "Contact", href: "/Contact", name: "Contact" },
   ];
   return (
-    <div className="bg-slate-200 flex items-center py-1 min-h-[var(--nav-h)]">
+    <div className="bg-slate-200 dark:bg-slate-800 flex items-center py-1 min-h-[var(--nav-h)] relative">
       <div className="container flex items-center flex-wrap justify-between gap-4">
         <Link href="/" className="flex-0">
           <h1 className="relative w-[70px] h-[70px]">
             <Image
-              className="absolute object-cover"
+              className="absolute object-cover dark:backdrop-invert"
               src="/logo.webp"
               fill
               alt="logo"
@@ -35,9 +36,9 @@ const Navbar = () => {
               <li
                 key={link.key}
                 className={
-                  "hover:font-semibold transition-all text-black " +
+                  "hover:font-semibold transition-all text-black dark:text-slate-100 " +
                   (isActive
-                    ? "font-semibold border-b-2 border-black"
+                    ? "font-semibold border-b-2 border-black dark:border-slate-50"
                     : "font-normal")
                 }
               >
@@ -46,6 +47,7 @@ const Navbar = () => {
             );
           })}
         </ul>
+        <ThemeToggle />
       </div>
     </div>
   );
