636a4d02ef0b7f6c4dcb8921361a5fb8468c36d1
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-10 18:03:02 +0200

    check if DB already connected before create a new one

--- src/lib/config/db.ts
+++ src/lib/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 export const ConnectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
   try {
     await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("DB connected :)");
