c36b254c40b72b5c2890f024758b2f7aae27fa63
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-15 01:16:18 +0200

    protect search route and search for todos related to current user

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,19 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ConnectDB } from "@/lib/config/db";
 import { TodoModel } from "@/lib/config/models/TodoModel";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 const LoadDb = async () => {
   await ConnectDB();
 };
 LoadDb();
 
 export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const createdByUserId = session?.user?.id;
+
+  if (!session) {
+    return NextResponse.json({ status: 401, error: "Unauthorized" });
+  }
   const searchParams = req.nextUrl.searchParams;
-  const query = (searchParams.get("query") as string) || "";
+  const query = searchParams.get("query") as string;
   const limit = (searchParams.get("limit") as string) || "5";
   try {
     const regex = new RegExp(query, "i");
     const todos = await TodoModel.find({
-      $or: [{ title: regex }, { description: regex }],
+      $and: [
+        { $or: [{ title: regex }, { description: regex }] },
+        { createdBy: createdByUserId },
+      ],
     }).limit(parseInt(limit));
     const AllTodosLoaded = todos.length < parseInt(limit);
     return NextResponse.json({ todos, AllTodosLoaded });
