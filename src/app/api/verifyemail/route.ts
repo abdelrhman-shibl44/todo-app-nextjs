14d944108f7c56788ffe56986501aedf4e01a6a5
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-18 01:15:53 +0200

    create verify route

---
+++ src/app/api/verifyemail/route.ts
@@ -0,0 +1,38 @@
+// src/app/api/users/verifyemail/route.ts
+
+import { ConnectDB } from "@/lib/config/db";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/lib/config/models/UserModel";
+
+const loadDb = async () => {
+  await ConnectDB();
+};
+loadDb();
+
+export async function POST(request: NextRequest) {
+  try {
+    // It extracts the token property from the JSON body of the incoming request.
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    // Update user properties and save the changes
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+
+    return NextResponse.json({
+      message: "Email Verified successfully",
+      success: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
