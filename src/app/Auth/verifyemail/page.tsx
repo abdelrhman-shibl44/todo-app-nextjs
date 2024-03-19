deb0f541f69e27a94df26c9493575832c4a361dd
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2024-03-19 18:27:50 +0200

    add loading until verify message apear

--- src/app/Auth/verifyemail/page.tsx
+++ src/app/Auth/verifyemail/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import SpinnerLoading from "@/components/Todo/SpinnerLoading";
 
 export default function VerifyEmailPage() {
   const [token, setToken] = useState("");
@@ -33,25 +34,29 @@ export default function VerifyEmailPage() {
   return (
     <Card title="Activate Account">
       <h1 className="text-4xl"></h1>
-      {verified && (
-        <div className="text-center dark:text-slate-50 mx-auto w-fit flex flex-col gap-4">
-          <Image
-            className="text-center mx-auto"
-            width={64}
-            height={64}
-            src="/check-mark.png"
-            alt="checkmark"
-          />
-          <h2 className="text-2xl">Email Verified</h2>
-          <p>Your email address was successfuly verified</p>
-          <Link
-            className="font-bold bg-slate-50 dark:bg-slate-800 rounded-md p-2 px-4 mx-auto w-full text-center shadow-lg hover:shadow-slate-700/50 dark:hover:shadow-orange-500/50 duration-300"
-            href="/Auth/login"
-          >
-            Login
-          </Link>
-        </div>
-      )}
+      <div className="text-center dark:text-slate-50 mx-auto w-fit flex flex-col gap-4">
+        {verified ? (
+          <>
+            <Image
+              className="text-center mx-auto"
+              width={64}
+              height={64}
+              src="/check-mark.png"
+              alt="checkmark"
+            />
+            <h2 className="text-2xl">Email Verified</h2>
+            <p>Your email address was successfuly verified</p>
+            <Link
+              className="font-bold bg-slate-50 dark:bg-slate-800 rounded-md p-2 px-4 mx-auto w-full text-center shadow-lg hover:shadow-slate-700/50 dark:hover:shadow-orange-500/50 duration-300"
+              href="/Auth/login"
+            >
+              Login
+            </Link>
+          </>
+        ) : (
+          <SpinnerLoading />
+        )}
+      </div>
 
       {error && (
         <div className="text-2xl text-center rounded-md dark:text-slate-50 font-semibold bg-orange-500/80  dark:bg-orange-500/20 p-4 relative">
